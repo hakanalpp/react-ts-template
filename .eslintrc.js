387b6ddc7dd470fa1915db8457f3cba9133767c1
hakanalp <hakana926@gmail.com>
2021-01-29 12:45:57 +0300

    eslint prettier configurations

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  globals: {
+    JSX: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['warn', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,48 +1,43 @@
-import React from "react";
-import "./Home.css";
+import './Home.css';
 
 const Home = (): JSX.Element => {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          React template made with{" "}
+          React template made with{' '}
           <a
             className="App-link"
             href="https://www.typescriptlang.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Typescript
           </a>
-          ,{" "}
+          {', '}
           <a
             className="App-link"
             href="https://eslint.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Eslint
           </a>
-          {" and "}
+          {' and '}
           <a
             className="App-link"
             href="https://prettier.io/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Prettier
           </a>
           .
         </p>
         <p>
-          Made by{" "}
+          Made by{' '}
           <a
             className="App-link"
             href="https://github.com/hakanalpp"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Hakan Alp
           </a>
         </p>
